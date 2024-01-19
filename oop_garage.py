1efedd534bd03e7e4d3e3ee530ea5344aee5fd37
IzzyVIII <eagb08@gmail.com>
2024-01-19 11:41:15 -0600

    first commit W3project

---
+++ oop_garage.py
@@ -0,0 +1,24 @@
+# Here are some guidelines on constructing your Garage. Your Garage does not need to do exactly this.
+# BUT! It should be robust enough for the user to interact with as if they were entering and leaving a parking garage. 
+# - take_ticket
+# - This should decrease the amount of tickets available by 1
+# - This should decrease the amount of parking_spaces available by 1
+# - pay_for_parking
+# - Display an input that waits for an amount from the user and store it in a variable
+# - If the payment variable is not empty then (meaning the ticket has been paid) -> display a message to the user that their ticket has been paid and they have 15mins to leave
+# - This should update the "current_ticket" dictionary key "paid" to True
+# -leave_garage
+# - If the ticket has been paid, display a message of "Thank You, have a nice day"
+# - If the ticket has not been paid, display an input prompt for payment
+# - Once paid, display message "Thank you, have a nice day!"
+# - Update parking_spaces list to increase by 1 (meaning add to the parkingSpaces list)
+# - Update tickets list to increase by 1 (meaning add to the tickets list)
+
+# Some example attributes:
+# - tickets -> list
+# - parking_spaces -> list
+# - current_ticket -> dictionary
+
+class Parking_garage():
+
+    def __init__():
